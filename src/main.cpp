fb13a8e8e452992862341d330d2c6414f2b9507b
patrik.vavercak <patrik.vavercak@innovatrics.com>
2022-08-06 15:18:59 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "miniaudio.h"
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  SDL_Init(SDL_INIT_VIDEO);
+  return 0;
+}
